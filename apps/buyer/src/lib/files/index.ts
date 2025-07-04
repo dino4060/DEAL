d4365a3839e6db2ddd7604e6d160e1dc468246c6
tn9th8 <tn9th8@gmail.com>
2025-07-04 12:29:16 +0700

    fix image

--- apps/buyer/src/lib/files/index.ts
+++ apps/buyer/src/lib/files/index.ts
@@ -1,13 +1,28 @@
-import { env } from '../env';
+import { env } from "../env";
 
 /**
  * Create URL to access files from backend
  */
-export function getFileUrl(fileName: string, resource: string, id: string | number): string {
-    if (!fileName) throw new Error('>>> getFileUrl: fileName is empty');
+export function getFileUrl(
+  fileName: string,
+  resource: string,
+  id: string | number
+): string {
+  if (!fileName) throw new Error(">>> getFileUrl: fileName is empty");
 
-    const domain = env.BACKEND_URL;
-    const endpoint = env.FILES_ENDPOINT;
+  const domain = env.BACKEND_URL;
+  const endpoint = env.FILES_ENDPOINT;
 
-    return `${domain}${endpoint}${resource}/${id}/${encodeURIComponent(fileName)}`;
+  return `${domain}${endpoint}${resource}/${id}/${encodeURIComponent(
+    fileName
+  )}`;
+}
+
+export function getFile(fileName: string): string {
+  if (!fileName) throw new Error(">>> getFileUrl: fileName is empty");
+
+  const domain = env.BACKEND_URL;
+  const endpoint = env.FILES_ENDPOINT;
+
+  return `${domain}${endpoint}/${encodeURIComponent(fileName)}`;
 }
